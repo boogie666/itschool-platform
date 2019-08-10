badf8e17b0f11e5fa783400b72f1575a4f56fc7a
silviu182005 <silviu182005@users.noreply.github.com>
2019-08-10 10:55:57 +0100

    Create Files, Request files

--- src/com/itschool/platform/core/Request.java
+++ src/com/itschool/platform/core/Request.java
@@ -2,4 +2,13 @@ package com.itschool.platform.core;
 
 public class Request {
 
+	private final String path;
+	
+	public String getPath() {
+		return path;
+	}
+
+	public Request(String p) {
+		this.path = p;
+	}
 }
---
+++ src/com/itschool/platform/files/StaticFileInteractor.java
@@ -0,0 +1,14 @@
+package com.itschool.platform.files;
+
+import com.itschool.platform.core.Interactor;
+import com.itschool.platform.core.Request;
+import com.itschool.platform.core.Response;
+
+public class StaticFileInteractor implements Interactor {
+
+	public Response execute(Request r) {
+		
+		return null;
+	}
+
+}
