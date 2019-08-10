0bb833089bd5d8e64a14ba392eee5252135d8fde
silviu182005 <silviu182005@users.noreply.github.com>
2019-08-10 10:29:32 +0100

    Create interfaces, Create Jetty Adapter.

--- src/com/itschool/platform/Main.java
+++ src/com/itschool/platform/Main.java
@@ -2,10 +2,15 @@ package com.itschool.platform;
 
 import org.eclipse.jetty.server.Server;
 
+import com.itschool.platform.util.FakeInteractor;
+import com.itschool.platform.util.HandlerAdapter;
+
 public class Main {
 
 	public static void main(String[] args) throws Exception {
 		Server s = new Server(8080);
+		
+		s.setHandler(new HandlerAdapter(new FakeInteractor()));
 		s.start();
 		s.join();
 		
